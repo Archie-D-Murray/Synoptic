d2061c6e42d4797d715a0fd62f086355f036a3fe
Archie-D-Murray <146730177+Archie-D-Murray@users.noreply.github.com>
2026-04-11 02:45:46 +0100

    clean: Formatting

--- Assets/Examples/Scripts/InjectorManager.cs
+++ Assets/Examples/Scripts/InjectorManager.cs
@@ -100,6 +100,7 @@ namespace AI.Examples {
         }
 
     }
+
     [Serializable]
     public class ManagerStateDefinitions : IStateDefinition {
         public void InitInjectors(StateMachineContext ctx) {
